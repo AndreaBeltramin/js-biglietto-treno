f3e2d006479d724ec435b47cc71a3c6f1f4a5315
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-18 00:41:08 +0200

    fix output message+ approccio lezione

--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@
 //!RACCOLTA DATI:
 
 //chiedo il numero di km che vuole percorrere
-let totKm = parseInt(prompt("Quanti km vuoi percorrere?"));
+const totKm = parseInt(prompt("Quanti km vuoi percorrere?"));
 //chiedo l'età del passeggero
-let userAge = parseInt(prompt("Quanti anni hai?"));
+const userAge = parseInt(prompt("Quanti anni hai?"));
 //preparo una variabile di output
 let outputMessage;
 
@@ -33,17 +33,16 @@ const isUserAgeValid = userAge > 0;
 
 //!ELABORAZIONE:
 
-//Scrivo messaggio di output e alert se i km inseriti sono <0 cioè non è valido
+//SE i km inseriti sono <0 cioè non è valido scrivo messaggio di output e alert
 if (!isTotKmValid) {
 	outputMessage = "Non puoi inserire questi Km, riprova";
 	alert("Questi Km non possono essere inseriti, prova di nuovo");
-} else if (
-	//Scrivo messaggio di output e alert se l'età inserita è <0 cioè non è valida
-	!isUserAgeValid
-) {
+}
+//ALTRIMENTI SE l'età inserita è <0 cioè non è valida scrivo messaggio di output e alert
+else if (!isUserAgeValid) {
 	outputMessage = "Non puoi inserire questa età, riprova";
 	alert("Questa età non può essere inserita, prova di nuovo");
-} //calcolo il prezzo del biglietto in base a quanti km vuole fare
+} // ALTRIMENTI calcolo il prezzo del biglietto in base a quanti km vuole fare
 else {
 	ticketPrice = totKm * 0.21;
 	//stampo la variabile prezzo biglietto e il suo tipo
@@ -56,7 +55,7 @@ else {
 
 	// calcolo il prezzo del biglietto in base allo sconto:
 
-	//calcolo il prezzo scontato se l'età è valida e non è ne <18 ne >65
+	//calcolo il prezzo scontato SE l'età è valida E non è <18 E  >65 E i km sono validi
 	if (!isUserAgeValid20 && !isUserAgeValid40 && isTotKmValid) {
 		outputMessage =
 			"Non hai diritto a nessuno sconto, dovrai pagare " +
@@ -65,27 +64,27 @@ else {
 		console.log("In basa alla tua età, non hai diritto a nessuno sconto");
 	}
 
-	//calcolo il prezzo scontato se l'età < 18
+	//calcolo il prezzo scontato SE l'età < 18
 	if (isUserAgeValid20) {
 		//calcolo lo sconto del 20%
 		const discountPrice20 = (ticketPrice * 2) / 100;
 		//stampo lo sconto del 20%
-		console.log("Lo sconto ammonta a € " + discountPrice20);
+		console.log("Lo sconto ammonta a € " + discountPrice20.toFixed(2));
 		outputMessage =
-			"Il prezzo scontato sarà di € " +
-			(ticketPrice.toFixed(2) - discountPrice20);
+			"Visto lo sconto del 20% riservato agli under 18, il prezzo sarà di € " +
+			(ticketPrice - discountPrice20).toFixed(2);
 	}
 
-	//calcolo il prezzo scontato se l'età > 65
+	//calcolo il prezzo scontato SE l'età > 65
 	if (isUserAgeValid40) {
 		//calcolo lo sconto del 40%
 		const discountPrice40 = (ticketPrice * 4) / 100;
 		//stampo lo sconto del 40%
-		console.log("Lo sconto ammonta a € " + discountPrice40);
+		console.log("Lo sconto ammonta a € " + discountPrice40.toFixed(2));
 
 		outputMessage =
-			"Il prezzo scontato sarà di € " +
-			(ticketPrice.toFixed(2) - discountPrice40);
+			"Visto lo sconto del 40% riservato agli over 65, il prezzo sarà di €  " +
+			(ticketPrice - discountPrice40).toFixed(2);
 	}
 }
 
@@ -95,3 +94,76 @@ else {
 console.log(outputMessage);
 //invio messaggio di alert con prezzo del biglietto da pagare
 alert(outputMessage);
+
+/* !!!! ALTRO APPROCCIO !!!!
+//!RACCOLTA DATI
+//chiedi (number) numero km
+const userDistance = parseInt(prompt("Quanti km percorrerai?"));
+
+//chiedi (number) età
+const userAge = parseInt(prompt("Quanti anni hai?"));
+
+//prepara (number) prezzo, sconto, prezzo finale, sconto percentuale over65, sconto percentuale under 18
+let price = 0;
+let discountEur = 0;
+let finalPrice = 0;
+const discountPercUnder18 = 20;
+const discountPercOver65 = 40;
+let pricePerKm = 0.21;
+
+//prepara (string) messaggio output
+let outputMessage = "";
+
+//!ELABORAZIONE
+//calcola validità numero di km
+const isUserDistanceValid = !isNaN(userDistance) && userDistance > 0;
+
+//calcola validità età
+const isUserAgeValid = !isNaN(userAge) && userAge >= 0;
+
+//SE tutti i valori sono validi
+if (isUserDistanceValid && isUserAgeValid) {
+	//calcolo il prezzo
+	price = userDistance * pricePerKm;
+
+	//setta il messaggio di output prezzo pieno
+	outputMessage = `Il prezzo del biglietto è di € ${price.toFixed(2)}`;
+
+	//SE l'età del passeggero è <18
+	if (userAge < 18) {
+		//calcolo lo sconto minorenni
+		discountEur = (price * discountPercUnder18) / 100;
+		finalPrice = price - discountEur;
+		//setto messaggio di output sconto per i minorenni
+		outputMessage += `
+In quanto Under 18 hai diritto ad uno sconto del ${discountPercUnder18}%. 
+Lo sconto è pari ad un ammontare di € ${finalPrice.toFixed(2)}.
+Il prezzo finale è pari a € ${finalPrice.toFixed(2)}.`;
+	}
+
+	//SE il passeggero è >65
+	if (userAge > 65) {
+		//calcola lo sconto over65
+		discountEur = (price * discountPercOver65) / 100;
+		finalPrice = price - discountEur;
+		//setto messaggio di output sconto per over65
+		outputMessage += `
+In quanto Over 65 hai diritto ad uno sconto del ${discountPercOver65}%. 
+Lo sconto è pari ad un ammontare di € ${finalPrice.toFixed(2)}.
+Il prezzo finale è pari a € ${finalPrice.toFixed(2)}.`;
+	}
+} else {
+	//setta il messaggio di output di errore
+	outputMessage = "Errore";
+	if (!isUserDistanceValid) {
+		outputMessage += `\n- distanza non valida`;
+	}
+	if (!isUserAgeValid) {
+		outputMessage += `\n- età non valida`;
+	}
+}
+
+//!OUTPUT
+//stampa il messaggio
+alert(outputMessage);
+*/
